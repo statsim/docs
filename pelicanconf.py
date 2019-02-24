2f5b70791de2d33064d534c0864b0a81997fa261
zemlyansky <anton.zemlyansky@gmail.com>
2019-02-24 20:14:45 +0200

    Update design, urls

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,15 +5,13 @@ from __future__ import unicode_literals
 AUTHOR = u'Anton Zemlyansky'
 SITENAME = u'StatSim Documentation'
 SITEURL = ''
-LOCALE = 'en_US.UTF-8'
 
 PATH = 'content'
 OUTPUT_PATH = 'docs'
 
 TIMEZONE = 'Europe/Paris'
-
+LOCALE = 'en_US.UTF-8'
 DEFAULT_LANG = u'en'
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -21,15 +19,20 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# STATIC_PATHS = (['.'])
+
 # Blogroll
-LINKS = (('StatSim', 'https://statsim.com/'),
-         ('Analyze.li', 'https://analyze.li'),
-        )
+LINKS = (
+  ('StatSim', 'https://statsim.com/'),
+  ('Analyze.li', 'https://analyze.li')
+)
 
 # Social widget
-SOCIAL = (('Twitter', 'https://twitter.com/statsimcom'),
-          ('Facebook', 'https://www.facebook.com/statsim'),
-          ('Github', 'https://github.com/statsim'))
+SOCIAL = (
+  ('Twitter', 'https://twitter.com/statsimcom'),
+  ('Facebook', 'https://www.facebook.com/statsim'),
+  ('Github', 'https://github.com/statsim')
+)
 
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['i18n_subsites', 'ipynb.markup']
@@ -38,14 +41,34 @@ I18N_SUBSITES = {
     'SITENAME': 'Справочник StatSim'
   }
 }
+
+IGNORE_FILES = ['*-checkpoint.ipynb', '*.md', '*.html']
+
 THEME = 'themes/pelican-blue'
 DISPLAY_FOOTER = False
 DISPLAY_SUMMARY = True
-ARTICLE_ORDER_BY = 'filename'
+ARTICLE_ORDER_BY = 'reversed-date'
 DEFAULT_PAGINATION = False
 
+ARTICLE_URL = '{slug}/'
+ARTICLE_SAVE_AS = '{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'category/index.html'
+
+AUTHOR_URL = 'author/{slug}/'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'author/index.html'
+
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tag/index.html'
+
+
 DELETE_OUTPUT_DIRECTORY = True
-IGNORE_FILES = ['*-checkpoint.ipynb', '*.md', '*.html']
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
